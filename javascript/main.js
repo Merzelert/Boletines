ed171314349e671191bc2abe15944be933f3aab8
Erik Ivan Retana Lopez <merzelert@gmail.com>
2022-06-19 16:33:42 -0500

    js

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,16 @@
-console.log("hola");
+/* console.log("hola");
 
 let links = document.querySelectorAll("a");
 
 links.forEach(function (link) {
-console.log(link);
-    
+    console.log(link);    
 })
+ */
+
+let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+    td.addEventListener('click',function(){
+        console.log(this);
+    })
+});
